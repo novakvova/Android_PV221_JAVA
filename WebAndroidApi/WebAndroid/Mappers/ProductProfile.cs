99cd5db0a0f95892f68bccdfb27bbd51284402b7
novakvova <novakvova@gmail>
2024-11-04 19:22:50 +0200

    Working Database is good products

---
+++ WebAndroidApi/WebAndroid/Mappers/ProductProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using WebAndroid.Data.Entities;
+using WebAndroid.DTO;
+using WebAndroid.Models;
+using WebAndroid.Models.Product;
+
+namespace WebAndroid.Mappers
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductEntity, ProductItemViewModel>()
+                .ForMember(x => x.Images, opt => opt.MapFrom(x => x.ProductImages
+                    .Select(p => p.Image).ToArray()));
+
+            CreateMap<ProductCreateViewModel, ProductEntity>();
+
+            CreateMap<ProductEditViewModel, ProductEntity>()
+                .ForMember(x => x.ProductImages, opt => opt.Ignore());
+
+        }
+    }
+}
